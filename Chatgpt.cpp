d33c6b63c38a5a6612503e1a99d5639c3a9f2520
Rafael Ferreira <rafael2002ferreira@gmail.com>
2023-04-04 20:18:23 +0100

    RafaMeu

--- Chatgpt.cpp
+++ Chatgpt.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-int maxProfit(vector<int>& prices, int N, int X) {
-    int n = prices.size();
-    vector<int> dp(n, 0);
-
+int main()
+{
+    int prices[5] = {100, 20, 40, 200, 170};
     int max_profit = 0;
-    int min_price = prices[0];
+    int transaction_cost = 10;
 
-    for (int i = 1; i < n; i++) {
-        min_price = min(min_price, prices[i]);
+    for (int buy_day = 0; buy_day < 5; buy_day++)
+    {
+        int num_actions = 0;
+        int profit = 0;
 
-        if (i >= N) {
-            min_price = min(min_price, prices[i - N] + X);
+        for (int sell_day = buy_day + 1; sell_day < 5; sell_day++)
+        {
+
+            if (num_actions == 3)
+            {
+                for (int i = sell_day; i < 5; i++)
+                {
+                    if (3 * prices[i] > 3 * prices[sell_day] + 3 * transaction_cost)
+                    {
+                        profit += 3 * prices[i] - (3 * prices[sell_day] + 3 * transaction_cost);
+                    }
+                    cout << "  COMEÇO AQUI : " << prices[i] << endl;
+                    cout << "  COMEÇO AQUI : (profit)" << profit << endl;
+                }
+
+                num_actions = 0;
+            }
+
+            if (3 * prices[sell_day] > 3 * prices[buy_day] + 3 * transaction_cost && num_actions == 0)
+            {
+                cout << "Buy day: " << buy_day << " Sell day: " << sell_day << endl;
+                profit += 3 * prices[sell_day] - (3 * prices[buy_day] + 3 * transaction_cost);
+                cout << "Profit: " << profit << endl;
+                num_actions = 3;
+            }
         }
-
-        dp[i] = max(dp[i - 1], prices[i] - min_price);
-
-        if (i >= N && (i - N) % N == 0) {
-            max_profit = max(max_profit, dp[i - N] + prices[i] - min_price - X);
+        if (profit > max_profit)
+        {
+            max_profit = profit;
         }
     }
 
-    return max_profit;
-}
-
-int main() {
-    vector<int> prices = {100, 20, 40, 200, 170};
-    int N = 3;
-    int X = 10;
-
-    cout << "Maximum profit: " << maxProfit(prices, N, X) << endl;
+    cout << "Max profit: " << max_profit << endl;
 
     return 0;
 }
